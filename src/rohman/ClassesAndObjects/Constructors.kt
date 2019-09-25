b0447fd8101558dc8f40749f054cfeb55acfdd61
Rohmanseo <rohmann311@gmail.com>
2019-09-25 21:04:07 +0700

    Add second constructor

--- src/rohman/ClassesAndObjects/Constructors.kt
+++ src/rohman/ClassesAndObjects/Constructors.kt
@@ -6,9 +6,24 @@ class Constructors constructor(firstName: String)
 
 class Constructors2(firstName: String)
 
+//Different way to define construktor
+
+class Constructor3{
+    private val firstName: String
+
+    constructor(firstName: String){
+        this.firstName = firstName
+    }
+
+    fun getName(): String = firstName
+}
+
 fun main(){
 
     val constructors: Constructors = Constructors("Rohman")
     val constructors2: Constructors2 = Constructors2("Rohman")
+    val constructors3: Constructor3 = Constructor3("Rohman")
+
+    println(constructors3.getName())
 
 }
